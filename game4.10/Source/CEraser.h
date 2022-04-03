550263f090b56c47a9865705522e8e57b847b545
phoebbbbe <leelemons1999@gmail.com>
2022-04-03 17:21:13 +0800

    apu game

---
+++ game4.10/Source/CEraser.h
@@ -0,0 +1,52 @@
+namespace game_framework {
+	/////////////////////////////////////////////////////////////////////////////
+	// 這個class提供可以用鍵盤或滑鼠控制的擦子
+	// 看懂就可以改寫成自己的程式了
+	/////////////////////////////////////////////////////////////////////////////
+
+	class CEraser
+	{
+	public:
+		CEraser();
+		int  GetX1();					// 擦子左上角 x 座標
+		int  GetY1();					// 擦子左上角 y 座標
+		int  GetX2();					// 擦子右下角 x 座標
+		int  GetY2();					// 擦子右下角 y 座標
+		void Initialize();				// 設定擦子為初始值
+		void LoadBitmap();				// 載入圖形
+		void OnMove();					// 移動擦子
+		void OnShow();					// 將擦子圖形貼到畫面
+		void SetMovingDown(bool flag);	// 設定是否正在往下移動
+		void SetMovingLeft(bool flag);	// 設定是否正在往左移動
+		void SetMovingRight(bool flag); // 設定是否正在往右移動
+		void SetMovingUp(bool flag);	// 設定是否正在往上移動
+		void SetFightUp(bool flag);
+		void SetFightDown(bool flag);
+		void SetFightBack(bool flag);
+		void SetFightFront(bool flag);
+		void SetXY(int nx, int ny);		// 設定擦子左上角座標
+	protected:
+		CAnimation animation;		// 擦子的動畫
+		CAnimation animationRunUp;
+		CAnimation animationRunDown;
+		CAnimation animationRunBack;
+		CAnimation animationRunFront;
+		CAnimation animationFightUp;
+		CAnimation animationFightDown;
+		CAnimation animationFightBack;
+		CAnimation animationFightFront;
+		CAnimation animationFail;
+		CAnimation animationSuccess;
+		CMovingBitmap bmpInitApu;
+		int curState = 0;
+		int x, y;					// 擦子左上角座標
+		bool isFightUp; // 是否正在往上攻擊
+		bool isFightDown; // 是否正在往下攻擊
+		bool isFightBack; // 是否正在往後攻擊
+		bool isFightFront; // 是否正在往前攻擊
+		bool isMovingDown;			// 是否正在往下移動
+		bool isMovingLeft;			// 是否正在往左移動
+		bool isMovingRight;			// 是否正在往右移動
+		bool isMovingUp;			// 是否正在往上移動
+	};
+}
---
+++ game4.10/Source/resource.h
@@ -0,0 +1,112 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 產生的 Include 檔案。
+// 由 game.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_SOCKETS_INIT_FAILED         104
+#define IDR_MAINFRAME                   128
+#define IDR_GAMETYPE                    129
+#define IDB_BALL                        130
+#define IDB_RACKET                      131
+#define IDB_ERASER1                     131
+#define IDB_BITMAP1                     132
+#define IDB_BACKGROUND                  132
+#define IDB_CORNER                      133
+#define IDB_BALL1                       134
+#define IDB_BALL2                       135
+#define IDB_BALL3                       136
+#define IDB_BALL4                       137
+#define IDB_0                           138
+#define IDB_1                           139
+#define IDB_2                           140
+#define IDB_3                           141
+#define IDB_4                           142
+#define IDB_5                           143
+#define IDB_6                           144
+#define IDB_7                           145
+#define IDB_8                           146
+#define IDB_9                           147
+#define IDB_MINUS                       148
+#define IDB_CENTER                      149
+#define IDB_ERASER2                     150
+#define IDB_ERASER3                     151
+#define IDB_CONTINUE                    152
+#define IDC_GAMECURSOR                  153
+#define IDB_HELP                        155
+#define IDB_BITMAP2                     156
+#define IDB_LOADING                     156
+#define IDB_BITMAP3                     157
+#define IDB_BITMAP4                     158
+#define IDB_GREEN                       159
+#define IDB_BLUE                        160
+#define IDB_BACKMANU                    163
+#define IDB_FLOOR2                      164
+#define IDB_FLOOR1                      167
+#define IDB_APU_LOSE                    171
+#define IDB_BAT                         172
+#define IDB_CLIP                        173
+#define IDB_GHOST1                      174
+#define IDB_GHOST1_2                    175
+#define IDB_GRASS                       176
+#define IDB_GRASS2                      177
+#define IDB_MAP1                        179
+#define IDB_MAP2                        180
+#define IDB_MAP3                        181
+#define IDB_MAP4                        182
+#define IDB_MAP5                        183
+#define IDB_PAGE1                       184
+#define IDB_PAGE2                       186
+#define IDB_TRAP                        187
+#define IDB_MAP_FINAL                   188
+#define IDB_BITMAP6                     189
+#define IDB_BAT2                        189
+#define IDB_APU_FRONT1                  190
+#define IDB_APU_FRONT2                  191
+#define IDB_APU_FRONT3                  192
+#define IDB_APU_FRONT4                  193
+#define IDB_APU_BACK1                   194
+#define IDB_APU_BACK2                   195
+#define IDB_APU_BACK3                   196
+#define IDB_APU_BACK4                   197
+#define IDB_APU_DOWN1                   198
+#define IDB_APU_DOWN2                   199
+#define IDB_APU_DOWN3                   200
+#define IDB_APU_DOWN4                   201
+#define IDB_APU_UP1                     202
+#define IDB_APU_UP2                     203
+#define IDB_APU_UP3                     204
+#define IDB_BITMAP5                     205
+#define IDB_APU_UP4                     205
+#define IDB_BITMAP7                     206
+#define IDB_TREE                        206
+#define IDB_FIGHT_UP1                   207
+#define IDB_FIGHT_UP2                   208
+#define IDB_FIGHT_UP3                   209
+#define IDB_FIGHT_DOWN1                 210
+#define IDB_FIGHT_DOWN2                 211
+#define IDB_FIGHT_DOWN3                 212
+#define IDB_FIGHT_BACK1                 213
+#define IDB_FIGHT_BACK2                 214
+#define IDB_FIGHT_BACK3                 215
+#define IDB_FIGHT_FRONT1                216
+#define IDB_FIGHT_FRONT2                217
+#define IDB_BITMAP9                     218
+#define IDB_FIGHT_FRONT3                218
+#define IDC_README                      1001
+#define ID_FILE_PAUSE                   32771
+#define ID_TOGGLE_FULLSCREEN            32772
+#define ID_BUTTON_FULLSCREEN            32773
+#define ID_BUTTON_PAUSE                 32774
+#define ID_BUTTON_UNITTEST              32775
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        219
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
